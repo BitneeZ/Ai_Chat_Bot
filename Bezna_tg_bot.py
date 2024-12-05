8f833afe90b3af4db99710a386097a81623b6293
IP3RFECTI <144429392+IP3RFECTI@users.noreply.github.com>
2024-12-05 20:21:02 +0300

    "tg_bot"

---
+++ Bezna_tg_bot.py
@@ -0,0 +1,10 @@
+import telebot
+
+bot = telebot.TeleBot('7467372440:AAH_4eA_UdxhHcQrkPdgJf1cGIEE1bBuoMI')
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, 'Привет, это телеграмм бот AI_Bezna.')
+
+
+bot.polling(none_stop=True)
