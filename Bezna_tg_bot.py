cd7e9434e83b28f74fd0f15cff3a9367077ce939
IP3RFECTI <144429392+IP3RFECTI@users.noreply.github.com>
2024-12-06 04:15:40 +0300

    Великолепные изменения

--- Bezna_tg_bot.py
+++ Bezna_tg_bot.py
@@ -1,16 +1,15 @@
 import telebot
-
+from ai_engine import *
 bot = telebot.TeleBot('7467372440:AAH_4eA_UdxhHcQrkPdgJf1cGIEE1bBuoMI')
-from joblib import load
-toxic = load('sgd_model.joblib')
-emotion = load('emotions_AI.joblib')
+
 @bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, 'Привет, это телеграмм бот AI_Bezna.')
 
 @bot.message_handler()
 def main(message):
-    bot.send_message(message.chat.id, (toxic.predict(message)))
+    bot.send_message(message.chat.id, toxic_pred(message))
+    bot.send_message(message.chat.id, emotion_pred(message))
 
 # @bot.message_handler()
 # def main(message):
--- ai_engine.py
+++ ai_engine.py
@@ -1,5 +1,10 @@
 from joblib import load
 toxic = load('toxic.joblib')
 emotion = load('emotions_AI.joblib')
-message = ["Ты молодец"]
-print(toxic.predict(message))
+def toxic_pred(msg):
+    res = toxic.predict(list((msg)))
+    return res
+
+def emotion_pred(msg):
+    res = emotion.predict(list((msg)))
+    return res
