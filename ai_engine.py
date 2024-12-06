11c7c421a8af4f57c6330864af2ce6ea5fc515f9
IP3RFECTI <144429392+IP3RFECTI@users.noreply.github.com>
2024-12-06 04:16:05 +0300

    Великолепные изменения

--- ai_engine.py
+++ ai_engine.py
@@ -2,9 +2,9 @@ from joblib import load
 toxic = load('toxic.joblib')
 emotion = load('emotions_AI.joblib')
 def toxic_pred(msg):
-    res = toxic.predict(list((msg)))
+    res = toxic.predict(list(msg))
     return res
 
 def emotion_pred(msg):
-    res = emotion.predict(list((msg)))
+    res = emotion.predict(list(msg))
     return res
