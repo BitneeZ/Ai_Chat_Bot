c296d5598dfb3d9f49622f4dd9489591cde701bc
IP3RFECTI <144429392+IP3RFECTI@users.noreply.github.com>
2024-12-10 11:21:56 +0300

    Первая рабочая версия

--- Bezna_tg_bot.py
+++ Bezna_tg_bot.py
@@ -1,7 +1,7 @@
 import telebot
 from ai_engine import *
 bot = telebot.TeleBot('7467372440:AAH_4eA_UdxhHcQrkPdgJf1cGIEE1bBuoMI')
-
+#
 @bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, 'Привет, это телеграмм бот AI_Bezna.')
@@ -11,7 +11,7 @@ def main(message):
 @bot.message_handler()
 def main(message):
     bot.send_message(message.chat.id, toxic_pred(str(message.text)))
-    bot.send_message(message.chat.id, emotion_pred(str(message.text)))
+    #bot.send_message(message.chat.id, emotion_pred(str(message.text)))
 
 # @bot.message_handler()
 # def main(message):
